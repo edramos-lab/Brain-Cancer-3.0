aa37fecacf769b21dd624c3b2baa93e4fff0f01d
Edgar Ramos <edgar.rene.ramos.acosta@uabc.edu.mx>
2024-11-12 17:45:01 -0800

    args updated

--- train.py
+++ train.py
@@ -240,8 +240,8 @@ if __name__ == "__main__":
     parser.add_argument('--batch', type=int, default=32, help='Batch size')
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
     parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
-    parser.add_argument('--dataset-ratio', type=float, default=0.3, help='Ratio of the dataset to use for training and testing')
-    parser.add_argument('--k-folds', type=int, default=3, help='Number of k-folds for cross-validation')
+    parser.add_argument('--dataset_ratio', type=float, default=0.3, help='Ratio of the dataset to use for training and testing')
+    parser.add_argument('--k_folds', type=int, default=3, help='Number of k-folds for cross-validation')
 
     args = parser.parse_args()
     train_and_evaluate(args)
